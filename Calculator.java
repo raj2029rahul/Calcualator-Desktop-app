6db7d7268e5dd03a1db7466236213fe377fea3fd
raj2029rahul <49182758+raj2029rahul@users.noreply.github.com>
2021-07-13 11:43:55 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,11 @@
 
-public class JavaApplication4 {
+public class Calculator {
 
     public static void main(String[] args) {
         // TODO code application logic here
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame4().setVisible(true);
+                new CalculatorDesign().setVisible(true);
             }
         });
     }
