0721a5ea1c1a99cb83298a6878b196d38f487b3e
raj2029rahul <49182758+raj2029rahul@users.noreply.github.com>
2021-07-13 11:36:45 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+
+public class JavaApplication4 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new NewJFrame4().setVisible(true);
+            }
+        });
+    }
+    
+}
